0914fc98dbb44e64607d2caca60ba785711897c1
prabhasadivi <prabhasadivi123@gmail.com>
2019-09-21 16:26:05 -0400

    Decimal To binary - Prabhas

--- acsl2019/src/DecimalToBinary.java
+++ acsl2019/src/DecimalToBinary.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class DecimalToBinary {
     public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
+    System.out.println("Enter the decimal to get converted to binary");
     int a = s.nextInt();
     if(a>128){
         System.out.print(1);
