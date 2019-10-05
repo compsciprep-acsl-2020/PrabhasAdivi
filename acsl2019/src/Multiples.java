20c37c3f6cb911bcd2a34a587a04f1751435cadb
prabhasadivi <prabhasadivi123@gmail.com>
2019-10-05 14:43:27 -0400

    Celcius and Farenhit stuff-Prabhas adivi ::

---
+++ acsl2019/src/Multiples.java
@@ -0,0 +1,58 @@
+public class Multiples {
+}
+
+
+public class ProjectEuler_MultiplesOf3And5 {
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+        System.out.println("Type Max Value: ");
+        int maxNumber = s.nextInt();
+
+        int sNUM= 0;
+
+
+        for(int i = 0; i < maxNumber; i = i + 1) {
+            int multiple3= multiple3(i);
+            int multiple5 = multiple5(i);
+            if(multiple3 != 0) {
+
+                sNUM = sNUM+ i.multiple3;
+
+            } else if(multiple5 != 0) {
+
+                sNUM = sNUM+ multiple5;
+            } else {
+                //default
+                sNUM= sNUM + 0;
+            }
+        }
+
+        System.out.println(sNUM);
+
+    }
+    public static int multiple3(int givenNumber) {
+        int finalNumber = 0;
+
+        if(givenNumber % 3 == 0) {
+            finalNumber = givenNumber;
+
+        } else {
+            finalNumber = 0;
+        }
+
+        return finalNumber;
+    }
+
+
+    public static int multiple5(int givenNumber) {
+        int finalNumber = 0;
+
+        if(givenNumber % 5 == 0) {
+            finalNumber = givenNumber;
+
+        } else {
+            finalNumber = 0;
+        }
+        return finalNumber;
+    }
+}
